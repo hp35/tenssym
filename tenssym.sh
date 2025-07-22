8dc8b2184cb1e81f2f29d00a9b809c32e703ad88
Fredrik Jonsson <fredrik.jonsson@mycronic.com>
2025-07-22 16:57:25 +0200

    Adding auto-generation of rank-level markdown README:s in the catalogue structure of the atlas.

--- tenssym.sh
+++ tenssym.sh
@@ -7,11 +7,32 @@
 # Copyright (C) 2023, Fredrik Jonsson, under GPL 3.0. See enclosed LICENSE.
 #
 PYTHON="python3"
-catdir="atlas"        # The catalogue of tensor elements to be generated
+catdir="atlas"      # The name of the catalogue to be generated
 
 #
-# Complete listing of the crystallographic point-symmetry groups
-# supported by the TENSSYM Python program.
+# Define the tensor ranks for which the atlas is to be generated. The min/max
+# construction is something which in the future should be altered to instead
+# get the values directly from the tensorRanks variable.
+#
+ptensorRanks=( 2 3 4 5 )
+tensorRanks=( 2 3 )
+minTensorRank=2
+maxTensorRank=5
+
+#
+# Define the types of tensors which for each rank should be considered.
+# (There only exists two types, polar or axial.)
+#
+tensorType=(
+    "polar"   # Polar ("true tensor")
+    "axial"   # Axial ("pseudotensor")
+)
+
+#
+# Define the sets of point-symmetry groups which should be used in the
+# generation of the atlas. In practice, this should exactly match the
+# complete list of the crystallographic point-symmetry groups as supported
+# by the TENSSYM Python program.
 #
 ppointSymmetryGroups=(
     "cubic_4bar3m"    # 43m cubic, inversion symmetry
@@ -33,16 +54,6 @@ pointSymmetryGroups=(
     "cubic_4bar3m"    # 43m cubic, inversion symmetry
     "cubic_23"        # 23 cubic, no inversion symmetry
 )
-
-tensorType=(
-    "polar"   # Polar ("true tensor")
-    "axial"   # Axial ("pseudotensor")
-)
-
-ptensorRanks=( 2 3 4 5 )
-tensorRanks=( 2 3 )
-minTensorRank=2
-maxTensorRank=5
 
 $PYTHON tenssym.py --symmetry trigonal_3 --type polar --rank 4
 
@@ -82,10 +93,10 @@ generate_atlas_root_readme() {
     rfile="$catdir/README.md"
     echo "# Atlas of nonzero elements of polar and axial tensors" > $rfile
     echo "" >> $rfile
-    echo "The following compilation of nonzero elements of polar and axial "\
-         "tensor elements was generated using the TensSym package "\
-	 "(https://github.com/hp35/tenssym/) and covers tensors of rank "\
-	 "$minTensorRank up to rank $maxTensorRank, for the following "\
+    echo "The following compilation of nonzero elements of polar and axial"\
+         "tensor elements was generated using the TensSym package"\
+	 "(https://github.com/hp35/tenssym/) and covers tensors of rank"\
+	 "$minTensorRank up to rank $maxTensorRank, for the following"\
 	 "point-symmetry groups:" >> $rfile
     for g in "${pointSymmetryGroups[@]}"; do
         echo "- $g" >> $rfile
@@ -104,31 +115,66 @@ generate_atlas_root_readme() {
     done
     echo "" >> $rfile
     echo "## References" >> $rfile
-    echo "1.   Susceptibility Tensors for Nonlinear Optics, S. V. Popov, Yu. "\
-         "P. Svirko and N. Zheludev (Institute of Physics Publishing, "\
+    echo "1.   Susceptibility Tensors for Nonlinear Optics, S. V. Popov, Yu."\
+         "P. Svirko and N. Zheludev (Institute of Physics Publishing,"\
          "Bristol, 1995). ISBN 0-7503-0253-4." >> $rfile
     echo "2. The Elements of Nonlinear Optics, P. N. Butcher and D. Cotter"\
          "(Cambridge University Press, Bristol, 1993). ISBN 0-521-42424-0."\
          >> $rfile
     echo "3. The Nonlinear Optics of Magneto-Optic Media (doctoral thesis),"\
          "Fredrik Jonsson (The Royal Institute of Technology, 2000),"\
-         "ISBN 91-7170-575-9; Available via the National Library of Sweden "\
-         "(Kungl. Biblioteket) at "\
+         "ISBN 91-7170-575-9; Available via the National Library of Sweden"\
+         "(Kungl. Biblioteket) at"\
          "http://urn.kb.se/resolve?urn=urn:nbn:se:kth:diva-2967" >> $rfile
-    echo "4. Patrick Steglich and Achim Kehrein, <em>Light propagation in "\
-         "anisotropic materials and electro-optical effects: tutorial on the "\
-         "use of eigenvalue problems, tensors, and symmetries</em>, J. Opt. "\
+    echo "4. Patrick Steglich and Achim Kehrein, <em>Light propagation in"\
+         "anisotropic materials and electro-optical effects: tutorial on the"\
+         "use of eigenvalue problems, tensors, and symmetries</em>, J. Opt."\
          "Soc. Am. B, Vol. 41, No. 9, pp. 2191--2210 (2024)." >> $rfile
-    echo "5. Herbert Goldstein, <em>Classical Mechanics</em>, 2nd Edn. "\
+    echo "5. Herbert Goldstein, <em>Classical Mechanics</em>, 2nd Edn."\
          "(Addison-Wesley, 1980), Ch. 4.4, pp. 146--147." >> $rfile
     echo "" >> $rfile
     echo "## Copyright" >> $rfile
-    echo "Copyright (C) 2023, Fredrik Jonsson, under GPL 3.0. "\
+    echo "Copyright (C) 2023, Fredrik Jonsson, under GPL 3.0."\
 	 "See enclosed LICENSE." >> $rfile
     echo "" >> $rfile
     echo "## Location of master source code" >> $rfile
-    echo "The source and documentation can be found at "\
+    echo "The source and documentation can be found at"\
 	 "https://github.com/hp35/tenssym" >> $rfile
+}
+
+#
+# Function for auto-generation of the various-rank's README.md of the
+# tensor atlas.
+#
+generate_atlas_rank_readmes() {
+    for r in "${tensorRanks[@]}"; do
+        directory="rank-$r"
+        rfile="$catdir/rank-$r/README.md"
+	echo $rfile
+        echo "# Nonzero elements of rank-$r polar and axial tensors" > $rfile
+        echo "" >> $rfile
+        echo "The following compilation of nonzero elements of polar (true)"\
+	     "and axial (pseudo) rank-$r tensor elements was generated using"\
+	     "the TensSym package (https://github.com/hp35/tenssym/)." >> $rfile
+        for t in "${tensorType[@]}"; do
+            subdirectory="$t"
+            echo "- Rank-$r $t tensor: [$subdirectory]($subdirectory)" >> $rfile
+        done
+        echo "" >> $rfile
+        echo "See https://github.com/hp35/tenssym for details and code"\
+	     "for the implementation of the TensSym package, and"\
+	     "https://github.com/hp35/tenssym/tree/main/atlas/ for the"\
+	     "complete atlas of nonzero and independent elements of other"\
+	     "ranks." >> $rfile
+        echo "" >> $rfile
+        echo "## Copyright" >> $rfile
+        echo "Copyright (C) 2023, Fredrik Jonsson, under GPL 3.0."\
+	     "See enclosed LICENSE." >> $rfile
+        echo "" >> $rfile
+        echo "## Location of master source code" >> $rfile
+        echo "The source and documentation can be found at"\
+             "https://github.com/hp35/tenssym" >> $rfile
+    done
 }
 
 #
@@ -136,3 +182,4 @@ generate_atlas_root_readme() {
 #
 generate_atlas
 generate_atlas_root_readme
+generate_atlas_rank_readmes
